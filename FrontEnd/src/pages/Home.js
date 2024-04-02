a87ca0298e61cc0b267513923ec86f814f540438
bendahmane <benaimgm1995@gmail.com>
2024-04-02 23:08:44 +0200

    modif page home

--- FrontEnd/src/pages/Home.js
+++ FrontEnd/src/pages/Home.js
@@ -1,21 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Navigation from "../components/Navigation";
 //import Logo from "../components/Logo";
-import Offres from "../components/Offres";
 import Banner from "../components/Banner";
 import Footer from "../components/Footer";
 import Thumb from "../components/Thumb";
 
+
 const Home = () => {
+  const [data, setData] = useState([]);
+
+  
+  // Le useEffect se joue lorsque le composant est monté
+  useEffect(() => {
+    axios.get("http://localhost:8080/api/properties")
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
   return (
     <div>
-      
       <Navigation />
       <Banner/>
-      <Offres/>
-      <Thumb/>
+      <section className="Thumb">
+    
+    {/* Afficher les données récupérées ici */}
+    {data.map((offre) => (
+     <Thumb 
+     key={offre.id}
+     cover={offre.cover}
+     title={offre.title}
+     />
+   ))}
+ </section>
       <Footer/>
-      
     </div>
   );
 };
